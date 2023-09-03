8e536558f3a172ec866cd6f9f3d0355169be8e68
apetroaei.claudiu99@yahoo.com <apetroaei.claudiu99@yahoo.com>
2023-09-03 16:49:13 +0300

    pushed files

---
+++ ShopOnline.Api/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Repository;
+using ShopOnline.Api.Repository.Contract;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection"))
+);
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(policy =>
+    policy.WithOrigins("htts://localhost:7269", "https://localhost:7269")
+    .AllowAnyMethod()
+    .WithHeaders(HeaderNames.ContentType)
+);
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ShopOnline.Web/Pages/ProductsBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Models.DTOs;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Pages
+{
+    public class ProductsBase:ComponentBase
+    {
+        [Inject]
+        public IProductService ProductService { get; set; } 
+        public IEnumerable<ProductDTO> Products { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Products = await ProductService.GetItems();
+        }
+
+        protected IOrderedEnumerable<IGrouping<int, ProductDTO>> GetGroupedProductsByCategory()
+        {
+            return from product in Products
+                   group product by product.CategoryId into prodByCatGroup
+                   orderby prodByCatGroup.Key
+                   select prodByCatGroup;
+        }
+
+        protected string GetCategoryName(IGrouping<int, ProductDTO> groupedProductDTO)
+        {
+            return groupedProductDTO.FirstOrDefault(pg => pg.CategoryId == groupedProductDTO.Key).CategoryName;
+        }
+    }
+}
